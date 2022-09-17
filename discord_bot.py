f45bc9c593a56db62bc08524921778058db467eb
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2022-09-17 23:10:22 +0500

    Working bot. Version 2.0.

---
+++ discord_bot.py
@@ -0,0 +1,461 @@
+import nest_asyncio
+# To fix "RuntimeError: This event loop is already running"
+nest_asyncio.apply()
+import discord
+from discord.ext import commands
+from discord_components import DiscordComponents, Button, ButtonStyle
+import asyncio
+import threading
+import chardet
+import time
+
+from game import *
+import game
+import discord_bot_configuration
+
+
+class player(player):
+    def __init__(self, user:discord.Member):
+        super().__init__(user.id, user.mention)
+        
+        self.user = user
+
+    async def send(self, message, components=None):
+        await self.user.send(message, components=components)
+
+
+def filled_iter(iterator, filling=None):
+    for i in iterator:
+        yield i 
+    while True:
+        yield filling
+    
+def generate_buttons(labels, styles=filled_iter((), 2), 
+                             urls=filled_iter(()), 
+                             disableds=filled_iter((), False), 
+                             emojis=filled_iter(())):
+    labels = iter(labels)
+    styles = iter(styles)
+    urls = iter(urls)
+    disableds = iter(disableds)
+    emojis = iter(emojis)
+    
+    buttons = []
+    for i in range(5):
+        row = []
+        for j in range(5):
+            try:
+                row.append(Button(label=next(labels), 
+                                  style=next(styles), 
+                                  url=next(urls), 
+                                  disabled=next(disableds), 
+                                  emoji=next(emojis)))
+            except StopIteration:
+                if row:
+                    buttons.append(row)
+                return buttons
+        buttons.append(row)
+    return buttons
+                    
+async def wait_for_answer(recipient, message=None, reactions=(), components=None, message_check=None, reaction_check=None, button_check=None, timeout=None, on_TimeoutError=None):
+    answer = None
+    msg = await recipient.send(message, components=components)
+    for r in reactions:
+        await msg.add_reaction(r)
+    
+    async def wait_for_message():
+        nonlocal answer
+        message = await bot.wait_for('message', check=message_check)
+        answer = message.content
+    async def wait_for_reaction_add():
+        nonlocal answer
+        reaction, _ = await bot.wait_for('reaction_add', check=reaction_check)
+        answer = reaction.emoji
+    async def wait_for_button_click():
+        nonlocal answer 
+        interaction = await bot.wait_for("button_click", check=button_check)
+        answer = interaction.component.label
+    
+    pending_tasks = [wait_for_message(), 
+                     wait_for_reaction_add(),
+                     wait_for_button_click()]
+    _, pending_tasks = await asyncio.wait(pending_tasks, 
+                                          timeout=timeout, 
+                                          return_when=asyncio.FIRST_COMPLETED)
+    for task in pending_tasks:
+        task.cancel()
+    
+    if answer:
+        #TODO return not str
+        return str(answer)
+        
+    raise asyncio.TimeoutError
+    
+
+bot = commands.Bot(command_prefix=discord_bot_configuration.PREFIX, 
+                   intents=discord.Intents.all())
+bot.remove_command('help')
+
+
+@bot.event
+async def on_ready():
+    DiscordComponents(bot)
+    
+    print('The bot is ready.')
+    
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('The command does not exist. Write "' + discord_bot_configuration.PREFIX + 'help" to get available commands.')
+    else:
+        raise error
+
+
+@bot.command()
+async def help(ctx):
+    await ctx.author.send(
+'''Godspeed.
+"Useful information"''')
+
+@bot.command()
+async def set_winning_score(ctx, score):
+    if score.isdigit():
+        winning_score = int(score)
+    else:
+        await ctx.author.send('Score is supposed to be a number.')
+
+@bot.command()п
+async def get_used_cards(ctx):
+    await ctx.author.send('\n'.join(used_cards))
+
+@bot.command()
+async def reset_used_cards(ctx):
+    used_cards = set()
+
+@bot.command()
+async def set_step_minutes(ctx, minutes):
+    if minutes.isdigit():
+        game.step_timeout = float(minutes)*60
+    else:
+        await ctx.author.send('"Score" supposed to be a number.')
+
+@bot.command()
+async def get_sourses(ctx):
+    if sourses:
+        await ctx.author.send('\n'.join(str(sourse) for sourse in sourses))
+    else:
+        await ctx.author.send('Sorry, there are no sources here yet.')
+
+@bot.command()
+async def reset_sourses(ctx):
+    sourses = set()
+
+@bot.command()
+async def add_sourses(ctx, *, message=''):
+    async def move_sourse(sourse):
+        if sourse:
+            try:
+                sourses.add(create_sourse_object(sourse))
+            except UnexpectedSourse:
+                await ctx.send('Sorry, there is somthing wrong with sourse: ' + sourse)
+            
+    await game.iterate_sourses(ctx, message, move_sourse)
+
+@bot.command()
+async def remove_sourse(ctx, *, message=''):
+    async def move_sourse(sourse):
+        if sourse:
+            try:
+                sourses.remove(sourse)
+            except KeyError:
+                await ctx.send('Sorry, there is no the sourse: ' + sourse)
+    
+    await game.iterate_sourses(ctx, message, move_sourse)
+
+@bot.command()
+async def get_players(ctx):
+    if players:
+        await ctx.author.send('Players: \n' + '\n'.join(str(player) for player in players))
+    else:
+        await ctx.send('There are no players.')
+
+@bot.command()
+async def join(ctx):
+    #TODO add person to queue for joining and move it to game.py
+    if game_started:
+        await ctx.send('You can\'t join right now, the game is started.')
+    elif ctx.author.id in players:
+        await ctx.send('You have already joined')
+    else:
+        players.append(player(ctx.author))
+        await ctx.send('Player ' + ctx.author.mention + ' has joined the game.')
+    
+@bot.command()
+async def leave(ctx):
+    if game_started:
+        await ctx.send('You can\'t leave the game now, it is started.')
+    else:
+        try:
+            players.remove(ctx.author.id)
+            await ctx.send('Players ' + ctx.author.mention + ' have left the game.')
+        except ValueError:
+            ctx.send('You have already left.')
+
+@bot.command()
+async def shuffle_players(ctx):
+    random.shuffle(players)
+    if players:
+        await ctx.send('Now you walk in the following order: ' + '\n' + '\n'.join(str(player) for player in players))
+    else:
+        await ctx.send('There are no any players.')
+
+
+def at_start():
+    asyncio.run(start.ctx.channel.send('The game has started.'))
+
+def at_round_start():
+    asyncio.run(start.ctx.channel.send('The round ' + str(game.round_number) + ' has started.'))
+
+def request_association():
+    def message_check(message):
+        if all((not message.author.bot, 
+                message.author.id == game.leader.id)): return True
+        return False
+                        
+    def button_check(interaction):
+        if all((not interaction.author.bot, 
+                interaction.author.id == game.leader.id)): return True
+        return False
+    
+    game.round_association = asyncio.run(wait_for_answer(game.leader, message='Did you tell the association of the round? Write it below or confirm it by pressing the button.', components=[Button(style=ButtonStyle.green, label='Yes', emoji='✅')], message_check=message_check, button_check=button_check))
+
+def show_association():
+    if game.round_association:
+        asyncio.run(start.ctx.channel.send('The association of the round is: ' + str(game.round_association)))
+
+def request_players_cards_2():
+    discarded_card = None
+    
+    def message_check(message):
+        try:
+            number = int(message.content)
+        except ValueError:
+            return False
+        if all((not message.author.bot, 
+                number >= 1, 
+                number <= cards_one_player_has, 
+                number != discarded_card)): return True
+        return False
+                        
+    def button_check(interaction):
+        try:
+            number = int(interaction.component.label)
+        except ValueError:
+            return False
+        if all((not interaction.user.bot, 
+                number >= 1, 
+                number <= cards_one_player_has, 
+                number != discarded_card)): return True
+        return False
+        
+    
+    for player in players:
+        for line in ('Choose the first card you want to... choose?..', 
+                     'Choose the second card you want to... choose?..'):
+            try:
+                card = int(asyncio.run(wait_for_answer(player, message=line +  '\n'.join(str(c) for c in player.cards), message_check=message_check, button_check=button_check, components=generate_buttons(range(1, cards_one_player_has+1)), timeout=game.step_timeout)))
+                asyncio.run(player.send('You has choosed the card ' + player.cards[card-1] + '.'))
+            except asyncio.TimeoutError:
+                card = random.randrange(cards_one_player_has)
+                asyncio.run(player.send('You was thinking too much. The card ' + player.cards[card-1] + ' was automatically selected for you.'))
+    
+            game.discarded_cards.append((player.cards[card-1], player.id))
+            discarded_card = card
+
+def request_leader_card():
+    def message_check(message):
+        try:
+            number = int(message.content)
+        except ValueError:
+            return False
+        if all((not message.author.bot, 
+                number >= 1, 
+                number <= cards_one_player_has)): return True
+        return False
+        
+    def button_check(interaction):
+        try:
+            number = int(interaction.component.label)
+        except ValueError:
+            return False
+        if all((not interaction.user.bot, 
+                number >= 1, 
+                number <= cards_one_player_has)): return True
+        return False
+        
+        
+    try:
+        card = int(asyncio.run(wait_for_answer(game.leader, message='You are a leader now. Choose number of one of your cards:\n' +  '\n'.join(str(c) for c in game.leader.cards), message_check=message_check, button_check=button_check, components=generate_buttons(range(1, cards_one_player_has+1)), timeout=game.step_timeout)))
+        asyncio.run(game.leader.send('You has choosed the card ' + game.leader.cards[card-1] + '.'))
+    except asyncio.TimeoutError:
+        card = random.randrange(cards_one_player_has)
+        asyncio.run(game.leader.send('You was thinking too much. The card ' + game.leader.cards[card-1] + ' was automatically selected for you.'))
+        
+    game.discarded_cards.append((game.leader.cards.pop(card-1), game.leader.id))
+
+def request_players_cards():
+    def message_check(message):
+        try:
+            number = int(message.content)
+        except ValueError:
+            return False
+        if all((not message.author.bot, 
+                number >= 1, 
+                number <= cards_one_player_has)): return True
+        return False
+                        
+    def button_check(interaction):
+        try:
+            number = int(interaction.component.label)
+        except ValueError:
+            return False
+        if all((not interaction.user.bot, 
+                number >= 1, 
+                number <= cards_one_player_has)): return True
+        return False
+        
+        
+    for player in players:
+        try:
+            card = int(asyncio.run(wait_for_answer(player, message='Choose the card you want to... Choose?..:\n' +  '\n'.join(str(c) for c in player.cards), message_check=message_check, button_check=button_check, components=generate_buttons(range(1, cards_one_player_has+1)), timeout=game.step_timeout)))
+            asyncio.run(player.send('You has choosed the card ' + player.cards[card-1] + '.'))
+        except asyncio.TimeoutError:
+            card = random.randrange(cards_one_player_has)
+            asyncio.run(player.send('You was thinking too much. The card ' + player.cards[card-1] + ' was automatically selected for you.'))
+            
+        game.discarded_cards.append((player.cards.pop(card-1), player.id))
+
+def vote_for_target_card_2():
+    def message_check(message):
+        try:
+            number = int(message.content)
+        except ValueError:
+            return False
+        if all((not message.author.bot,  
+                number >= 1, 
+                number <= cards_one_player_has, 
+                game.discarded_cards[int(message.content) - 1][1] != message.author.id)): return True
+        return False
+        
+    def button_check(interaction):
+        try:
+            number = int(interaction.component.label)
+        except ValueError:
+            return False
+        if all((not interaction.user.bot, 
+                number >= 1, 
+                number <= cards_one_player_has, 
+                game.discarded_cards[int(interaction.component.label) - 1][1] != interaction.user.id)): return True
+        return False
+        
+    
+    for player in players:
+        try:
+            card = int(asyncio.run(wait_for_answer(player, message='Choose the enemy\'s card: \n' + '\n'.join(str(c[0]) for c in game.discarded_cards), message_check=message_check, button_check=button_check, components=generate_buttons(range(1, len(game.discarded_cards)+1)), timeout=step_timeout)))
+            asyncio.run(player.send('You has choosed the card ' + game.discarded_cards[card-1][0] + '.'))
+        except asyncio.TimeoutError:
+            card = random.randint(1, len(players))
+            asyncio.run(player.send('You was thinking too much. The card ' + game.discarded_cards[card-1] + ' was automatically selected for you.'))
+        
+        game.votes_for_card[game.discarded_cards[card - 1][1]] += 1
+        player.choosed_card = card
+
+def vote_for_target_card():
+    def message_check(message):
+        try:
+            number = int(message.content)
+        except ValueError:
+            return False
+        if all((not message.author.bot,  
+                number >= 1, 
+                number <= cards_one_player_has, 
+                game.discarded_cards[int(message.content) - 1][1] != message.author.id)): return True
+        return False
+        
+    def button_check(interaction):
+        try:
+            number = int(interaction.component.label)
+        except ValueError:
+            return False
+        if all((not interaction.user.bot, 
+                number >= 1, 
+                number <= cards_one_player_has, 
+                game.discarded_cards[int(interaction.component.label) - 1][1] != interaction.user.id)): return True
+        return False
+        
+    
+    for player in players:
+        if player != game.leader:
+            try:
+                card = int(asyncio.run(wait_for_answer(player, message='Choose the enemy\'s card: \n' + '\n'.join(str(c[0]) for c in game.discarded_cards), message_check=message_check, button_check=button_check, components=generate_buttons(range(1, len(game.discarded_cards)+1)), timeout=step_timeout)))
+                asyncio.run(player.send('You has choosed the card ' + game.discarded_cards[card-1][0] + '.'))
+            except asyncio.TimeoutError:
+                card = random.randint(1, len(players))
+                asyncio.run(player.send('You was thinking too much. The card ' + game.discarded_cards[card-1] + ' was automatically selected for you.'))
+            
+            game.votes_for_card[game.discarded_cards[card - 1][1]] += 1
+            player.choosed_card = card
+
+def at_end():
+    game_took_time = time.time() - game.game_started_at
+    asyncio.run(start.ctx.send('The game took: ' + str(int(game_took_time // 60)) + ' minutes and ' + str(int(game_took_time % 60)) + ' seconds.'))
+    
+    if len(players) == 2:
+        if game.bot_score > game.players_score:
+            asyncio.run(start.ctx.send('You lose with score: ' + str(game.players_score)))
+        if game.players_score > game.bot_score:
+            asyncio.run(start.ctx.send('You win with score: ' + str(game.players_score)))
+        else:
+            asyncio.run(start.ctx.send('Победила дружба (сырок)!'))
+    else:
+        asyncio.run(start.ctx.send('The winners: \n' + 
+                                   '\n'.join((str(place) + '. ' + str(player) for place, player in enumerate(sorted(players)[:3], start=1)))))
+
+@bot.command()
+async def start(ctx):
+    start.ctx = ctx
+    
+    try:
+        game.start_game(at_start=at_start, 
+                        at_round_start=at_round_start, 
+                        request_association=request_association, 
+                        show_association=show_association, 
+                        request_players_cards_2=request_players_cards_2, 
+                        request_leader_card=request_leader_card, 
+                        request_players_cards=request_players_cards, 
+                        vote_for_target_card_2=vote_for_target_card_2, 
+                        vote_for_target_card=vote_for_target_card, 
+                        at_end=at_end)
+    except TypeError as error:
+        await ctx.send('The game cannot start yet. Specify all data and start the game again.')
+
+@bot.command()
+async def end(ctx):
+    pass            
+    
+@bot.command(name='pause')
+async def pause_game(ctx):
+    pass
+
+@bot.command(name='continue')
+async def continue_game(ctx):
+    pass
+
+@bot.command()
+async def test(ctx):
+    for i in (0, 1, 2, 22, 25, 27):
+        coms = generate_buttons(range(i))
+        await ctx.send('lol', components=coms)
+
+
+bot.run(discord_bot_configuration.TOKEN)
---
+++ game.py
@@ -0,0 +1,299 @@
+import random
+import collections
+import time 
+import math
+
+import validators
+import vk_api
+
+import game_configuration
+
+
+include_used_cards = False
+game_started = False
+step_timeout = 60
+used_cards = set()
+unused_cards = list()
+cards_one_player_has = 6
+sourses = set()
+players = list()
+winning_score = 5
+turn = 0
+included_types = ('photo', )
+excluded_types = ()
+
+
+class UnexpectedSourse(Exception):
+    pass
+class NoAnyPosts(Exception):
+    pass
+class EnvironmentError(Exception):
+    pass
+class GameOver(Exception):
+    pass
+
+class player:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        
+        self.cards = list()
+        self.discarded_cards = list()
+    def __hash__(self): return self.id
+    def __repr__(self): return self.name
+    def __str__(self): return self.name
+    def __eq__(self, x): return self.id == x
+    def __ne__(self, x): return self.id != x
+    def __gt__(self, x): return self.score > x
+    def __lt__(self, x): return self.score < x
+    def __ge__(self, x): return self.score >= x
+    def __le__(self, x): return self.score <= x
+    
+    def reset_features(self):
+        self.score = 0
+    
+    score = 0
+    choosed_card = None
+    
+
+vk_requests = vk_api.VkApi(token=game_configuration.VK_TOKEN).get_api()
+
+class vk:
+    def __init__(self, link):
+        self.link = link
+        self.domain = link[link.rfind(r'/')+1:]
+        self.included_types = {y for i in included_types if (y := vk.types.get(i))}
+        self.excluded_types = {y for i in excluded_types if (y := vk.types.get(i))}
+    def __eq__(self, other):
+        if self.link == other: return True
+        return False
+    def __str__(self):
+        return self.link
+    def __hash__(self):
+        return hash(self.link)
+    
+    # Change specified types to vk attachment types
+    types = {'photo':'photo', 
+             'video':'video'}
+    
+    def get_cards_num(self):
+        return vk_requests.wall.get(domain=self.domain, count=1)['count']
+    
+    def set_cards_num(self):
+        self.cards_num = self.get_cards_num()
+
+    def get_random_card(self):
+        def exctact_content_from_attachment(attachment):
+            if attachment['type'] == 'photo':
+                return attachment[attachment['type']]['sizes'][-1]['url']
+            elif attachment['type'] == 'video':
+                video_id = str(attachment[attachment['type']]['owner_id'])
+                video_id += '_' + str(attachment[attachment['type']]['id'])
+                return vk_requests.video.get(videos=video_id)['items'][0]['player']
+                    
+        self.set_cards_num()
+        if self.cards_num == 0: 
+            raise NoAnyPosts
+            
+        try:
+            attachments = vk_requests.wall.get(domain=self.domain, 
+                                               offset=random.randrange(self.cards_num), 
+                                               count=1)['items'][0]['attachments']
+        except KeyError:
+            return self.get_random_card()
+        
+        # If attachments are found, then get the random one
+        random.shuffle(attachments)
+        for attachment in attachments:
+            if attachment['type'] not in self.excluded_types:
+                if self.included_types: 
+                    if attachment['type'] not in self.included_types:
+                        continue
+                return exctact_content_from_attachment(attachment)
+        
+        return self.get_random_card()
+
+class link:
+    def __init__(self, link):
+        # return what link takes to
+        pass
+
+
+def all_elements_true(iterable, function=lambda x: x):
+    for i in iterable:
+        if not function(i): return False
+    return True
+
+async def iterate_sourses(ctx, message, function):
+    async def iterate_lines(text):
+        for sourse in text.replace('\r', '').split('\n'):
+            await function(sourse)
+            
+    await iterate_lines(message)
+    
+    for attachment in ctx.message.attachments:
+        filetype = game.extract_file_extension(attachment.filename)
+        
+        if filetype == 'txt':
+            text = await attachment.read()
+            await iterate_lines(text.decode(chardet.detect(text[:1000])['encoding']))
+        else:
+            await ctx.send('Sorry, the "' + filetype + '" filetype is not supproted.')
+            
+def extract_file_extension(filename):
+    return filename[filename.rfind('.')+1:]
+
+def create_sourse_object(sourse):
+    if validators.url(sourse):
+        domain_name = sourse[sourse.find('/')+2:]
+        domain_name = domain_name[:domain_name.find('/')]
+        domain_name = (domain_name := domain_name.split('.'))[math.ceil(len(domain_name)/2)-1]
+        
+        if domain_name == 'vk':
+            return vk(sourse) 
+        elif domain_name == 'discord':
+            pass
+        elif domain_name == 'instagram':
+            pass
+        elif domain_name == 'tiktok':
+            pass
+    elif validators.email(sourse):
+        pass
+    raise UnexpectedSourse('The link format is not supported or an unavailable link is specified.')
+
+def get_random_card(): 
+    try:
+        return random.choice(list(sourses)).get_random_card()
+    except NoAnyPosts:
+        return get_random_card()
+
+
+def empty_function():
+    pass
+
+def start_game(at_start=empty_function,  
+               at_circle_start=empty_function, 
+               at_round_start=empty_function, 
+               request_association=empty_function, 
+               show_association=empty_function, 
+               show_players_cards=empty_function, 
+               request_players_cards_2=empty_function, 
+               request_leader_card=empty_function, 
+               request_players_cards=empty_function, 
+               show_discarded_cards=empty_function, 
+               vote_for_target_card_2=empty_function, 
+               vote_for_target_card=empty_function, 
+               at_round_end=empty_function, 
+               at_circle_end=empty_function, 
+               at_end=empty_function):
+    if not sourses: raise TypeError('Sources are not specified.')
+    if len(players) <= 1: raise TypeError('There are not enough players to start.')
+    
+    global leader
+    global round_number
+    global discarded_cards
+    global votes_for_card
+    global game_started_at
+    
+    game_started_at = time.time()
+    bot_score = 0
+    players_score = 0
+    for player in players: 
+        player.reset_features()
+    game_started = True
+    
+    at_start()
+    
+    while True:
+        if not game_started: break
+        
+        at_circle_start()
+        
+        # Hand out cards
+        if len(players) >= 3:
+            for player in players:
+                player.cards = list()
+                for i in range(cards_one_player_has-1):
+                    player.cards.append(get_random_card())
+                
+        round_number = 1
+        for leader in players:
+            if not game_started: break
+            
+            at_round_start()
+            
+            votes_for_card = collections.defaultdict(int)
+            discarded_cards = list()
+            round_association = None 
+            if len(players) == 2:
+                for player in players:
+                    player.cards = [get_random_card() for i in range(cards_one_player_has)]
+            else:
+                for player in players:
+                    player.cards.append(get_random_card())
+            
+            if len(players) == 2:
+                # Discard the nobody's card
+                discarded_cards.append((get_random_card(), None))
+                request_association()
+                show_association()
+                show_players_cards()
+                request_players_cards_2()
+            else:
+                if len(players) == 3:
+                    for i in range(2):
+                        discarded_cards.append((get_random_card(), None))
+                show_players_cards()
+                request_leader_card()
+                request_association()
+                show_association()
+                request_players_cards()
+            
+            random.shuffle(discarded_cards)
+            
+            show_discarded_cards()
+            if len(players) == 2:
+                vote_for_target_card_2()
+            else:
+                vote_for_target_card()
+            
+            if len(players) == 2:
+                if votes_for_card[None] == 2:
+                    players_score += 2
+                elif votes_for_card[None] == 1:
+                    players_score += 1
+                    bot_score += 1
+                elif votes_for_card[None] == 0:
+                    bot_score += 3 
+            else:
+                if votes_for_card[leader.id] not in (0, len(players)):
+                    leader.score += 3
+                    for player in players:
+                        if player != leader:
+                            if discarded_cards[player.choosed_card - 1][0] == leader.id:
+                                player.score += 3
+                    for player in players:
+                        player.score += votes_for_card[player.id]
+            
+            at_round_end()
+            
+            round_number += 1
+            
+        at_circle_end()
+        
+        # Check for win
+        if len(players) == 2:
+            if max(bot_score, players_score) >= winning_score:
+                game_started = False
+        else:
+            if all_elements_true(players, lambda p: not p.score < winning_score):
+                game_started = False
+    
+    at_end()
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
