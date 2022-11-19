e821565102fe6e855c3c3683ed87716ac2343028
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2022-11-19 18:37:53 +0500

    The cogs have been moved to the directory with the "main.py" file.

--- bots/discord_bot/configuration.py
+++ bots/discord_bot/configuration.py
@@ -1,2 +1,7 @@
 PREFIX = '.'
 DOWNLOADS_PATH = r'C:\"Yandex disk"\Arsentiy\Programs\Programming\Applications\Imaginarium bot\saved_files'
+PATH_TO_COGS_DIRECTORY = '.'
+COGS_NAMES = ['gameplay',
+              'getting_game_information',
+              'listeners',
+              'setting_up_game']
--- bots/discord_bot/main.py
+++ bots/discord_bot/main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 import nest_asyncio
 
 # The "next_asyncio.apply()" have to be called before "discord" import
@@ -12,6 +14,9 @@ import configuration
 
 dotenv.load_dotenv()
 
+# Add directory with cogs to search for
+sys.path.append(configuration.PATH_TO_COGS_DIRECTORY)
+
 bot = commands.Bot(command_prefix=configuration.PREFIX,
                    intents=discord.Intents.all())
 bot.remove_command('help')
@@ -19,47 +24,48 @@ bot.remove_command('help')
 
 @bot.command()
 async def load_extension(ctx, extension):
-    bot.load_extension(f'cogs.{extension}')
+    bot.load_extension(extension)
 
 
 @bot.command()
 async def unload_extension(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
+    bot.unload_extension(extension)
 
 
 @bot.command()
 async def reload_extension(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
-    bot.load_extension(f'cogs.{extension}')
+    bot.unload_extension(extension)
+    bot.load_extension(extension)
 
 
 def get_extensions():
-    return [filename[:-3] for filename in os.listdir(r'.\cogs') if
-            filename.endswith('.py') and not filename.startswith('__')]
+    return [filename[:-3] for filename in os.listdir(configuration.PATH_TO_COGS_DIRECTORY)
+            if all((filename[:-3] in configuration.COGS_NAMES,
+                    filename.endswith('.py')))]
 
 
 @bot.command()
 async def load_extensions(ctx):
     for extension in get_extensions():
-        bot.load_extension(f'cogs.{extension}')
+        bot.load_extension(extension)
 
 
 @bot.command()
 async def unload_extensions(ctx):
     for extension in get_extensions():
-        bot.unload_extension(f'cogs.{extension}')
+        bot.unload_extension(extension)
 
 
 @bot.command()
 async def reload_extensions(ctx):
     for extension in get_extensions():
-        bot.unload_extension(f'cogs.{extension}')
-        bot.load_extension(f'cogs.{extension}')
+        bot.unload_extension(extension)
+        bot.load_extension(extension)
 
 
 def main():
-    for cog in get_extensions():
-        bot.load_extension(f'cogs.{cog}')
+    for extension in get_extensions():
+        bot.load_extension(extension)
 
     bot.run(os.environ['DISCORD_BOT_TOKEN'])
 
