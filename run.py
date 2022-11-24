826af2f39cc85ed9b0dddb0e50f9093b510fc093
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2022-11-24 17:17:29 +0500

    The "if __name__ == '__main__'" construction has been added to "run.py" file.

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
 def discord_import():
-    from bots.discord_bot.main import main
-    main()
+    import bots.discord_bot.main
+    bots.discord_bot.main.main()
 
 
 bots_run_scripts = {'Discord': discord_import}
 
-while True:
-    try:
-        bots_run_scripts[input('Specify the bot you want to run: ')]()
-    except KeyError:
-        print('There are no bots with that name here. \n' +
-              'Type one of the following: \n' +
-              ' — ' + '\n — '.join(bots_run_scripts.keys()) + '\n')
+
+def main():
+    while True:
+        try:
+            bots_run_scripts[input('Specify the bot you want to run: ')]()
+        except KeyError:
+            print('There are no bots with that name here. \n' +
+                  'Type one of the following: \n' +
+                  ' — ' + '\n — '.join(bots_run_scripts.keys()) + '\n')
+
+
+if __name__ == '__main__':
+    main()
