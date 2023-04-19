254b347171a23ca3c360fe71e7c1613c71e961b0
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2023-04-19 23:27:39 +0500

    Change docstrings of exceptions

--- Imaginarium/exceptions.py
+++ Imaginarium/exceptions.py
@@ -1,43 +1,43 @@
 class UnsupportedSource(ValueError):
-    """Raised when the source is not supported."""
+    """Exception raised when the source is not supported."""
     pass
 
 
 class InvalidSource(UnsupportedSource):
-    """Raised when the source does not work."""
+    """Exception raised when the source does not work."""
     pass
 
 
 class NoAnyPosts(LookupError, InvalidSource):
-    """Raised when there are no posts in the source."""
+    """Exception raised when there are no posts in the source."""
     pass
 
 
 class GameIsStarted(Exception):
-    """Raised when trying to change something that cannot be changed during the game."""
+    """Exception raised when trying to change something that cannot be changed during the game."""
     pass
 
 
 class GameIsEnded(Exception):
-    """Raised when trying to change something that cannot be changed after the game."""
+    """Exception raised when trying to change something that cannot be changed after the game."""
     pass
 
 
 class NoAnyUsedSources(Exception):
-    """Raised when there are no sources to use."""
+    """Exception raised when there are no sources to use."""
     pass
 
 
 class NotEnoughPlayers(TypeError):
-    """Raised when there are not enough players."""
+    """Exception raised when there are not enough players."""
     pass
 
 
 class PlayerAlreadyJoined(ValueError):
-    """Raised when the player is already joined the game."""
+    """Exception raised when the player is already joined the game."""
     pass
 
 
 class PlayerAlreadyLeft(ValueError):
-    """Raised when the player is already left the game."""
+    """Exception raised when the player is already left the game."""
     pass
